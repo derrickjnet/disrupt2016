2c3d177adf7d313e735c4c5145f11cbe3b2a6f82
Adam Chew <adamkchew@gmail.com>
2016-09-10 18:46:01 -0700

    Adding basic braintree magic

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 8080 });
+var braintree = require("braintree");
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+var BraintreeClient = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "2f4zy99wv9btxr3t",
+  publicKey: "dfnj9h8hk4ygz4zj",
+  privateKey: "0a38e5eddc9cd06683a1dea18bc4654f"
+});
+
+var express = require('express');
+var app = express();
+
+var products = [{id: 1, name: 'Tee-shirt', amount: '15.00'}, {id: 2, name: 'Pants', amount: '30.00'}, {id: 3, name: 'Socks', amount: '7.00'}];
+
+app.get("/products", function(req, res, next) {
+  return res.json(products);
+});
+
+app.get("/braintree/token", function(req, res, next) {
+  BraintreeClient.clientToken.generate({}, function(err, response) {
+    res.json({token: response.clientToken});
   });
+});
 
-  ws.send('something');
+app.post("/checkout", function(req, res, next) {
+  var nonceFromTheClient = req.body.payment_method_nonce;
+
+  BraintreeClient.transaction.sale({
+    amount: "10.00",
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      submitForSettlement: true
+    }
+  }, function(err, result) {
+
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Listening');
 });
